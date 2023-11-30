4b75134943f1cd79bb289c94b975b0bf10c2be80
Rik Sprenkels <spr3nk3ls@gmail.com>
2023-11-30 22:21:52 +0100

    Util method for complete files instead of lines.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        Util.applyToFile("src/hai.txt", lines -> lines.forEach(System.out::println));
+        Util.applyToLines("src/hai.txt", lines -> lines.forEach(System.out::println));
+        Util.applyToFile("src/hai2.txt", content -> Stream.of(content.split("\n\n")).map(lines -> lines + "\nsplit").forEach(System.out::println));
     }
 }
--- src/Util.java
+++ src/Util.java
@@ -5,7 +5,16 @@ import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class Util {
-    public static void applyToFile(String filename, Consumer<Stream<String>> consumer){
+    public static void applyToFile(String filename, Consumer<String> consumer){
+        try {
+            var content = Files.readString(Paths.get(filename));
+            consumer.accept(content);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static void applyToLines(String filename, Consumer<Stream<String>> consumer){
         try(var lines = Files.lines(Paths.get(filename))){
             consumer.accept(lines);
         } catch (IOException e){
