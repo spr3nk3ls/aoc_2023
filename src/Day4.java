15dd22ef73a263a26e22850229b563f183203350
Rik Sprenkels <spr3nk3ls@gmail.com>
2023-12-04 14:37:22 +0100

    Day 4 simpler.

--- src/Day4.java
+++ src/Day4.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 public class Day4 {
 
     record Card(List<Integer> winning, List<Integer> have) {}
+
     public static void main(String[] args) {
         calculate1("src/day4/example.txt");
         calculate1("src/day4/input.txt");
@@ -16,31 +17,29 @@ public class Day4 {
     private static void calculate1(String filename) {
         Util.applyAndPrint(filename, lines -> lines.map(Day4::getCard).map(card -> {
                 long count = card.winning.stream().filter(card.have::contains).count();
-                if(count == 0){
-                    return 0;
-                }
-                return (int)Math.pow(2, count-1);
+                return count == 0 ? 0 : (int)Math.pow(2, count-1);
             }
         ).reduce(0, Integer::sum).toString());
     }
-
     private static void calculate2(String filename) {
         Util.applyAndPrint(filename, lines -> {
-            var list = lines.map(Day4::getCard).toList();
-            var llist = list.stream().map(l -> new ArrayList<>(List.of(l))).toList();
-            IntStream.range(0, list.size()).forEach(i -> llist.get(i).forEach(card -> {
-                var count = (int)card.winning.stream().filter(card.have::contains).count();
-                IntStream.range(i+1, i+count+1).forEach(j -> llist.get(j).add(list.get(j)));
-            }));
-            return llist.stream().map(ArrayList::size).reduce(0, Integer::sum).toString();
+            var cards = lines.map(Day4::getCard)
+                .map(card -> (int)card.winning.stream().filter(card.have::contains).count())
+                .toList();
+            var won = IntStream.generate(() -> 1).limit(cards.size()).toArray();
+            IntStream.range(0, cards.size()).forEach(i ->
+                IntStream.range(i + 1, i + 1 + cards.get(i)).forEach(j -> won[j] += won[i])
+            );
+            return String.valueOf(Arrays.stream(won).reduce(0, Integer::sum));
         });
     }
 
     private static Card getCard(String line){
         var splitLine = line.split(":")[1].split("\\|");
-        return new Card(
-            Util.split(splitLine[0].strip(), "\\s+").map(Integer::parseInt).collect(Collectors.toList()),
-            Util.split(splitLine[1].strip(), "\\s+").map(Integer::parseInt).collect(Collectors.toList())
-        );
+        return new Card(stripToInt(splitLine[0]), stripToInt(splitLine[1]));
+    }
+
+    private static List<Integer> stripToInt(String input){
+        return Util.split(input.strip(), "\\s+").map(Integer::parseInt).collect(Collectors.toList());
     }
 }
