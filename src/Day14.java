09ebcc622c1743603dd47b90f5c3e5eb78e00827
Rik Sprenkels <spr3nk3ls@gmail.com>
2023-12-17 19:27:09 +0100

    Day 14 imports, Day 17 prettier.

--- src/Day14.java
+++ src/Day14.java
@@ -1,4 +1,3 @@
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
--- src/Day17.java
+++ src/Day17.java
@@ -18,16 +18,42 @@ public class Day17 {
         }
     }
 
+    @FunctionalInterface
+    interface FilterFactory {
+        Predicate<Node> filter(Node source, int max_x, int max_y);
+    }
+
     public static void main(String[] args) {
-        Predicate<Node> aFilter = node -> node.steps < 3; 
-        calculate_a("src/day17/example.txt", aFilter);
-        calculate_a("src/day17/input.txt", aFilter);
-        calculate_b("src/day17/example.txt");
-        calculate_b("src/day17/example2.txt");
-        calculate_b("src/day17/input.txt"); 
+        FilterFactory aFilter = (source, max_x, max_y) -> node -> node.steps < 4;
+        getMatrixAndPrint("src/day17/example.txt", aFilter, i -> true);
+        getMatrixAndPrint("src/day17/input.txt", aFilter, i -> true);
+        FilterFactory bFilter = (source, max_x, max_y) -> node -> {
+            if (node.direction.equals(source.direction))
+                return node.steps <= 10;
+            if (source.steps < 4)
+                return false;
+            switch (node.direction.x) {
+                case 1:
+                    return node.coordinates.x < max_x - 3;
+                case -1:
+                    return node.coordinates.x >= 3;
+                default:
+                    switch (node.direction.y) {
+                        case 1:
+                            return node.coordinates.y < max_y - 3;
+                        case -1:
+                            return node.coordinates.y >= 3;
+                        default:
+                            throw new RuntimeException();
+                    }
+            }
+        };
+        getMatrixAndPrint("src/day17/example.txt", bFilter, node -> node.steps >= 4);
+        getMatrixAndPrint("src/day17/example2.txt", bFilter, node -> node.steps >= 4);
+        getMatrixAndPrint("src/day17/input.txt", bFilter, node -> node.steps >= 4);
     }
 
-    private static void calculate_a(String filename, Predicate<Node> filter) {
+    private static void getMatrixAndPrint(String filename, FilterFactory filter, Predicate<Node> stepFilter) {
         Util.applyToFile(filename, asString -> {
             var matrix = Util.split(asString, "\n")
                     .map(line -> Util.split(line, "").map(Integer::parseInt).toArray(Integer[]::new))
@@ -35,36 +61,24 @@ public class Day17 {
             var max_x = matrix[0].length;
             var max_y = matrix.length;
 
-            var finalNodes = calculate(matrix, max_x, max_y, node -> getNextNodes_a(node, matrix, max_x, max_y, filter));
-            System.out.println(finalNodes.stream().map(Node::value).min(Comparator.naturalOrder()).get());
-        });
-    }
-
-    private static void calculate_b(String filename) {
-        Util.applyToFile(filename, asString -> {
-            var matrix = Util.split(asString, "\n")
-                    .map(line -> Util.split(line, "").map(Integer::parseInt).toArray(Integer[]::new))
-                    .toArray(Integer[][]::new);
-            var max_x = matrix[0].length;
-            var max_y = matrix.length;
-
-            var finalNodes = calculate(matrix, max_x, max_y, node -> getNextNodes_b(node, matrix, max_x, max_y));
+            var finalNodes = calculate(matrix, max_x, max_y,
+                    node -> getNextNodes(node, matrix, max_x, max_y, filter));
             System.out.println(finalNodes
-                .stream()
-                .filter(node -> node.steps >= 4)
-                .map(Node::value)
-                .min(Comparator.naturalOrder()).get());
+                    .stream()
+                    .filter(stepFilter)
+                    .map(Node::value)
+                    .min(Comparator.naturalOrder()).get());
         });
     }
 
-    private static Set<Day17.Node> calculate(Integer[][] matrix, int max_x, int max_y, Function<Node, Stream<Node>> nextFunction) {
+    private static Set<Day17.Node> calculate(Integer[][] matrix, int max_x, int max_y,
+            Function<Node, Stream<Node>> nextFunction) {
         var visited = IntStream.range(0, max_y)
                 .mapToObj(y -> IntStream.range(0, max_x).mapToObj(x -> new HashSet<Node>()).toList()).toList();
 
         var nodes = List.of(
-            new Node(new Coordinates(0, 0), 0, new Coordinates(1, 0), 0),
-            new Node(new Coordinates(0, 0), 0, new Coordinates(0, 1), 0)
-        );
+                new Node(new Coordinates(0, 0), 0, new Coordinates(1, 0), 0),
+                new Node(new Coordinates(0, 0), 0, new Coordinates(0, 1), 0));
         while (!nodes.isEmpty()) {
             nodes = nodes.stream()
                     .flatMap(node -> nextFunction.apply(node))
@@ -76,28 +90,8 @@ public class Day17 {
         return visited.get(max_y - 1).get(max_x - 1);
     }
 
-    private static Stream<Node> getNextNodes_a(Node source, Integer[][] matrix, int max_x, int max_y, Predicate<Node> filter) {
-        return Stream.of(
-                new Coordinates(-source.direction.y, source.direction.x),
-                new Coordinates(source.direction.y, -source.direction.x),
-                source.direction).filter(dir -> {
-                    var newCoordinates = source.coordinates.add(dir);
-                    if (newCoordinates.x < 0 || newCoordinates.x >= max_x)
-                        return false;
-                    if (newCoordinates.y < 0 || newCoordinates.y >= max_y)
-                        return false;
-                    return true;
-                }).map(direction -> {
-                    var newCoordinates = source.coordinates.add(direction);
-                    return new Node(
-                            newCoordinates,
-                            source.value + matrix[newCoordinates.y][newCoordinates.x],
-                            direction,
-                            direction.equals(source.direction) ? source.steps + 1 : 0);
-                }).filter(filter);    
-    }
-
-    private static Stream<Node> getNextNodes_b(Node source, Integer[][] matrix, int max_x, int max_y) {
+    private static Stream<Node> getNextNodes(Node source, Integer[][] matrix, int max_x, int max_y,
+            FilterFactory nodeFilter) {
         return Stream.of(
                 new Coordinates(-source.direction.y, source.direction.x),
                 new Coordinates(source.direction.y, -source.direction.x),
@@ -115,28 +109,7 @@ public class Day17 {
                             source.value + matrix[newCoordinates.y][newCoordinates.x],
                             direction,
                             direction.equals(source.direction) ? source.steps + 1 : 1);
-                })
-                .filter(node -> {
-                    if(node.direction.equals(source.direction))
-                        return node.steps <= 10;
-                    if(source.steps < 4)
-                        return false;
-                    switch (node.direction.x) {
-                        case 1:
-                            return node.coordinates.x < max_x - 3;
-                        case -1:
-                            return node.coordinates.x >= 3;
-                        default:
-                            switch(node.direction.y){
-                                case 1:
-                                    return node.coordinates.y < max_y - 3;
-                                case -1:
-                                    return node.coordinates.y >= 3;
-                                default:
-                                    throw new RuntimeException();
-                            }
-                    }
-                });
+                }).filter(nodeFilter.filter(source, max_x, max_y));
     }
 
     private static boolean hasNotBeenVisited(Node node, List<List<HashSet<Node>>> visited) {
