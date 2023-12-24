ba8890622cec146fa6d9985c685cdae25957b823
Rik Sprenkels <spr3nk3ls@gmail.com>
2023-12-24 18:03:15 +0100

    Day 23.

--- src/Day23.java
+++ src/Day23.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day23 {
@@ -40,48 +45,51 @@ public class Day23 {
     }
 
     public static void main(String[] args) {
-        Set<Character> allPossible = Stream.of(Direction.values()).flatMap(d -> d.possible().stream()).collect(Collectors.toSet());
-        calculate("src/day23/example.txt", (dir, ch) -> dir.possible().contains(ch));
-        calculate("src/day23/input.txt", (dir, ch) -> dir.possible().contains(ch));
-        calculate("src/day23/example.txt", (dir, ch) -> allPossible.contains(ch));
-        calculate("src/day23/input.txt", (dir, ch) -> allPossible.contains(ch));
+        Set<Character> allPossible = Stream.of(Direction.values()).flatMap(d -> d.possible().stream())
+                .collect(Collectors.toSet());
+        calculate_a("src/day23/example.txt", (dir, ch) -> dir.possible().contains(ch));
+        calculate_a("src/day23/input.txt", (dir, ch) -> dir.possible().contains(ch));
+        calculate_b("src/day23/example.txt", (dir, ch) -> allPossible.contains(ch));
+        calculate_b("src/day23/input.txt", (dir, ch) -> allPossible.contains(ch));
     }
 
-    private static void calculate(String filename, BiPredicate<Direction, Character> pred) {
+    private static void calculate_a(String filename, BiPredicate<Direction, Character> pred) {
         Util.applyToFile(filename, asString -> {
             var matrix = Util.split(asString, "\n")
                     .map(line -> line.toCharArray())
                     .toArray(size -> new char[size][]);
             List<Cd> firstSteps = new ArrayList<>(List.of(new Cd(1, 0), new Cd(1, 1)));
-            var currentRoutes = new HashSet<List<Cd>>();
-            var finishedRoutes = new HashSet<List<Cd>>();
-            currentRoutes.add(firstSteps);
-            while (!currentRoutes.isEmpty()) {
-                var newRoutes = new HashSet<List<Cd>>();
-                for (var route : currentRoutes) {
+            var finishedRoutes = new HashSet<Integer>();
+            var routes = new Stack<List<Cd>>();
+            routes.push(firstSteps);
+            while (!routes.isEmpty()) {
+                var route = routes.pop();
+                while (route != null) {
                     if (route.get(route.size() - 1).equals(new Cd(matrix[0].length - 2, matrix.length - 1))) {
-                        finishedRoutes.add(route);
-                        continue;
+                        finishedRoutes.add(route.size() - 1);
+                        route = null;
+                        break;
                     }
                     var possible = possibleSteps(route, matrix, pred);
                     if (possible.size() == 3) {
                         var newRoute = new ArrayList<>(route);
                         newRoute.add(possible.get(2));
-                        newRoutes.add(newRoute);
+                        routes.push(newRoute);
                     }
                     if (possible.size() > 1) {
                         var newRoute = new ArrayList<>(route);
                         newRoute.add(possible.get(1));
-                        newRoutes.add(newRoute);
+                        routes.push(newRoute);
                     }
                     if (possible.size() > 0) {
                         route.add(possible.get(0));
-                        newRoutes.add(route);
+                    }
+                    if (possible.size() == 0) {
+                        route = null;
                     }
                 }
-                currentRoutes = newRoutes;
             }
-            System.out.println(finishedRoutes.stream().map(r -> r.size()).max(Comparator.naturalOrder()).get() - 1);
+            System.out.println(finishedRoutes.stream().max(Comparator.naturalOrder()).get());
         });
     }
 
@@ -96,5 +104,125 @@ public class Day23 {
 
     private static Character fromMatrix(Cd cd, char[][] matrix) {
         return matrix[cd.y][cd.x];
+    }
+    
+    // Part b starts here
+
+    record Node(Cd middle, List<Cd> connections, Map<Node, Integer> distance) {
+        @Override
+        public boolean equals(Object other) {
+            if (other instanceof Node)
+                return this.middle.equals(((Node) other).middle);
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return middle.hashCode();
+        }
+    }
+
+    private static void calculate_b(String filename, BiPredicate<Direction, Character> pred) {
+        Util.applyToFile(filename, asString -> {
+            var matrix = Util.split(asString, "\n")
+                    .map(line -> line.toCharArray())
+                    .toArray(size -> new char[size][]);
+            var nodes = getNodes(matrix);
+            var start = new Node(new Cd(1, 0), new ArrayList<>(List.of(new Cd(1, 1))), new HashMap<>());
+            var end = new Node(
+                    new Cd(matrix[0].length - 2, matrix.length - 1),
+                    new ArrayList<>(List.of(new Cd(matrix[0].length - 2, matrix.length - 2))),
+                    new HashMap<>());
+            nodes.put(start.middle, start);
+            nodes.put(end.middle, end);
+            connectNodes(nodes.values(), matrix, pred);
+            var routes = getRoutes(start, end, nodes);
+            var lengths = getLengths(routes);
+            System.out.println(lengths.stream().max(Comparator.naturalOrder()).get());
+        });
+    }
+
+    private static Map<Cd, Node> getNodes(char[][] matrix) {
+        var list = IntStream.range(1, matrix.length - 1).boxed()
+                .flatMap(y -> IntStream.range(1, matrix[y].length - 1).mapToObj(x -> {
+                    if (matrix[y][x] == '.') {
+                        var connections = Stream
+                                .of(new Cd(x, y - 1), new Cd(x, y + 1), new Cd(x - 1, y), new Cd(x + 1, y))
+                                .filter(cd -> matrix[cd.y][cd.x] != '#').toList();
+                        if (connections.size() > 2)
+                            return new Node(new Cd(x, y), connections, new HashMap<>());
+                    }
+                    return null;
+                })).filter(i -> i != null).toList();
+        return list.stream().collect(Collectors.toMap(i -> i.middle, i -> i));
+    }
+
+    private static void connectNodes(Collection<Day23.Node> collection, char[][] matrix,
+            BiPredicate<Direction, Character> pred) {
+        for (var node : collection) {
+            for (var connection : node.connections) {
+                var firstSteps = new ArrayList<>(List.of(node.middle, connection));
+                while (true) {
+                    var possible = possibleSteps(firstSteps, matrix, pred);
+                    if (possible.size() != 1) {
+                        throw new RuntimeException();
+                    }
+                    var otherNode = collection.stream().filter(n -> n.middle.equals(possible.get(0))).findAny();
+                    if (otherNode.isPresent()) {
+                        node.distance.put(otherNode.get(), firstSteps.size());
+                        break;
+                    }
+                    firstSteps.add(possible.get(0));
+                }
+            }
+        }
+    }
+
+    private static Set<List<Day23.Node>> getRoutes(Node start, Node end, Map<Cd, Node> nodes){
+        var finished = new HashSet<List<Node>>();
+        var routes = new Stack<List<Node>>();
+        routes.add(new ArrayList<>(List.of(start)));
+        while (!routes.isEmpty()) {
+            var route = routes.pop();
+            while (true) {
+                if(route.get(route.size() - 1).equals(end)){
+                    finished.add(route);
+                    break;
+                }
+                var middles = route.stream().map(r -> r.middle()).toList();
+                var connect = route.get(route.size() - 1).distance.keySet().stream()
+                    .map(c -> c.middle)
+                    .filter(m -> !middles.contains(m))
+                    .toList();
+                if (connect.size() == 3) {
+                    var newRoute = new ArrayList<>(route);
+                    newRoute.add(nodes.get(connect.get(2)));
+                    routes.add(newRoute);
+                }
+                if (connect.size() > 1) {
+                    var newRoute = new ArrayList<>(route);
+                    newRoute.add(nodes.get(connect.get(1)));
+                    routes.add(newRoute);
+                }
+                if (connect.size() > 0) {
+                    route.add(nodes.get(connect.get(0)));
+                } else {
+                    break;
+                }
+            }
+        }
+        return finished;
+    }
+
+    private static Set<Integer> getLengths(Set<List<Node>> nodes){
+        var result = new HashSet<Integer>();
+        for(var nodeList : nodes){
+            int length = 0;
+            for(int i = 0; i < nodeList.size() -1; i++){
+                length += nodeList.get(i).distance.get(nodeList.get(i+1));
+            }
+            result.add(length);
+        }
+        return result;
     }
 }
